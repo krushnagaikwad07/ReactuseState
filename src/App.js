d6b071c7f57ec08d3b5b234ffa5d41a11154148a
krushnagaikwad07 <krushnagaikwad7799@gmail.com>
2023-03-11 22:45:48 +0530

    UseState 3 problems

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+
+
+
 function App() {
+  // const [list, setList]= usestate(userList);
+  const [name, setName] = useState("Amit");
+  const [input, setInput] = useState(0);
+  const[text,setText]=useState("");
+  const[list,setList]=useState([]);
+  
+  function handleChange(event){
+    setText(event.target.value)
+  }
+
+  const handleSubmit=(e)=>{
+  e.preventDefault();
+   console.log(text);
+   
+   setList([...list,text])
+   setText(" ")
+  }
+
+
+
+  function increase() {
+    setInput(input + 1)
+  }
+  function decrease() {
+    setInput(input - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+    < input type="text " placeholder="Text " value={text} onChange={handleChange} />
+    <button>Add</button>
+</form>
+{
+   <ul>
+     {list.map((elem)=>{
+       return(
+         <li>{elem}</li>
+       )
+     })}
+     </ul>
+}
+
+      <h1>My Name is {name}</h1>
+      <button onClick={()=> setName("Rajan")}>Change Name</button>
+      <h1>This is my first useState</h1>
+      <button onClick={increase} > Increse Count </button>
+      <button onClick={decrease}>Decrease Count</button>
+      
+      <p>This is ={input}</p>
+    </>
+
   );
 }
 
